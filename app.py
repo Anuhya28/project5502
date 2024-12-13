662a18641bf75bd381144a954731198cceb718c7
Anuhya28 <AnuhyaKoyyana@myunt.edu>
2024-12-13 17:17:11 -0600

    implemented all the changes

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-#gradio app
+# gradio app
 import gradio as gr
 from production.functions import predict_subscribe
+
+# def greet(name, intensity):
+#     return "Hello, " + name + "!" * int(intensity)
+
+# demo = gr.Interface(
+#     fn=greet,
+#     inputs=["text", "slider"],
+#     outputs=["text"],
+# )
+
+# demo.launch()
 
 watch = gr.Slider(minimum=-9, maximum=12, label="Watch")
 duration = gr.Slider(minimum=-4, maximum=4, label="Duration")
